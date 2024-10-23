777d7f5bda23588788058610ae00565d9c117cf7
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2024-10-23 15:18:22 -0400

    Timer and Toppings Logic

--- Assets/Scripts/Pizza Interactions/Baking/BakingTimer.cs
+++ Assets/Scripts/Pizza Interactions/Baking/BakingTimer.cs
@@ -1,41 +1,58 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BakingTimer : MonoBehaviour
 {
-    public Vector3 startingLength;
-
-    public GameObject timerGameObject;
+    public int countdownTime = 10;
+    public TextMeshPro timerText;
 
 
     private float growLength;
     void Start()
     {
+        timerText.text = "test";
     }
-
+    
+    
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Pizza"))
         {
-            StartTimer(other.gameObject);
+            StartCoroutine(StartTimer(other.gameObject)) ;
         }
     }
 
     //This could have functionality to start a timer that ticks up whenever pizza is in the ovens
-    void StartTimer(GameObject pizza)
-    {
-        //Getting reference to the specific pizzas is baked variable
-       PizzaInfo _pizzaInfo = pizza.GetComponent<PizzaInfo>();
+    
 
-       if (_pizzaInfo != null)
-       {
-           //if the pizza has this variable, it gets set to true
-           _pizzaInfo.isBaked = true;
-       }
+    IEnumerator StartTimer(GameObject pizza)
+    {
+        int currentTime = countdownTime;
         
+        //Getting reference to the specific pizzas is baked variable
+        PizzaInfo _pizzaInfo = pizza.GetComponent<PizzaInfo>();
+        while (currentTime > 0)
+        {
+            timerText.text = currentTime.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            currentTime--;
+        }
+        
+        
+        if (_pizzaInfo != null)
+        {
+            //if the pizza has this variable, it gets set to true
+            _pizzaInfo.isBaked = true;
+            timerText.text = "Pizza Baked";
+        }
+        
+
         //timerGameObject.gameObject.transform.localScale = (startingLength.x, startingLength.y, startingLength.z);
         //Get access to pizza info isBaked an
 
--- Assets/Scripts/Pizza Interactions/PizzaInfo.cs
+++ Assets/Scripts/Pizza Interactions/PizzaInfo.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PizzaInfo : MonoBehaviour
 {
@@ -8,5 +10,37 @@ public class PizzaInfo : MonoBehaviour
     
     //Bool for documenting bake status of pizza
     public bool isBaked = false;
-    
+    public int armTopping;
+    public int eyeballTopping;
+    public int legTopping;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Eyeball"))
+        {
+            eyeballTopping++;
+            DisableGrab(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Arm"))
+        {
+            armTopping++;
+            DisableGrab(other.gameObject);
+        } 
+        else if (other.gameObject.CompareTag("Leg"))
+        {
+            legTopping++;
+            DisableGrab(other.gameObject);
+        }
+        
+    }
+
+     void DisableGrab(GameObject topping)
+    {
+       XRGrabInteractable grabInt = topping.gameObject.GetComponent<XRGrabInteractable>();
+       if (grabInt != false)
+       {
+           grabInt.enabled = false;
+       }
+        
+    }
 }
