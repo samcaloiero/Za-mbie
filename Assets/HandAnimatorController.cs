ec5e6b95bab0034c80709a3645f2b644354468ec
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2024-10-29 15:27:50 -0400

    Hand Animation

---
+++ Assets/HandAnimatorController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimatorController : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty triggerAction;
+    [SerializeField] private InputActionProperty gripAction;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        float gripValue = gripAction.action.ReadValue<float>();
+
+        anim.SetFloat("Trigger", triggerValue);
+        anim.SetFloat("Grip", gripValue);
+    }
+
+}
