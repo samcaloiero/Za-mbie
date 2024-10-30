a9551f2591e49de5729e32588f4f255cfb2396ba
avendura <avendura@outlook.com>
2024-10-30 01:47:10 -0400

    import assets, set alpha of grate material off on trigger, to be added to baking script

---
+++ Assets/Scripts/OvenOff.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenOff : MonoBehaviour
+{
+    public Material grateMaterial;
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pizza"))
+        {
+            Color color = grateMaterial.color;
+            
+            color.a = 0;
+            
+            grateMaterial.color = color;
+        }  
+    }
+}
