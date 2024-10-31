84bc7fec9c1847dd9bdd4f831bdd5245e90e6795
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2024-10-31 10:28:08 -0400

    ajsdjsad

---
+++ Assets/DisplayOrder.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayOrder : MonoBehaviour
+{
+    public TextMeshPro orderText;
+
+    private PizzaInfo _pizzaInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pizzaInfo = GetComponent<PizzaInfo>()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       SetOrderText(_pizzaInfo); 
+    }
+    void SetOrderText(PizzaInfo thisPizzaInfo)
+    {
+        orderText.text = (thisPizzaInfo.armTopping + " Arm Toppings" + "    " + thisPizzaInfo.eyeballTopping + " Eyeball Toppings" + "    " +
+                          thisPizzaInfo.legTopping + " Leg Toppings");
+    }
+}
