ce7ccbb49ae64b4a2c1694b0637b7de9e899e80d
samcaloiero <122567577+samcaloiero@users.noreply.github.com>
2024-10-31 13:12:09 -0400

    adasda

--- Assets/DisplayOrder.cs
+++ Assets/DisplayOrder.cs
@@ -11,7 +11,7 @@ public class DisplayOrder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _pizzaInfo = GetComponent<PizzaInfo>()
+        _pizzaInfo = GetComponent<PizzaInfo>();
     }
 
     // Update is called once per frame
